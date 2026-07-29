25a1982d92d2df57be8569032f07db2944adbfe9
tasyagamilah12 <anastasiagamilah@students.amikom.ac.id>
2026-07-29 10:11:45 +0700

    fix: separate api/index.php and vercel.json

--- api/index.php
+++ api/index.php
@@ -5,16 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Load Composer Autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel Application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Arahkan storage folder ke /tmp (karena /tmp bisa ditulis di Vercel)
 $app->useStoragePath('/tmp/storage');
 
-// Buat struktur folder sementara di /tmp jika belum ada
 $dirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/sessions',
@@ -29,7 +25,6 @@ foreach ($dirs as $dir) {
     }
 }
 
-// Jalankan Kernel Laravel
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
