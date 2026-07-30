44c086ff6b470a3a7d587b5ea33e1c9f81043767
tasyagamilah12 <anastasiagamilah@students.amikom.ac.id>
2026-07-30 08:48:59 +0700

    Force HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') !== 'local') {
-        URL::forceScheme('https');
+            URL::forceScheme('https');
+        }
     }
-    }
-
 }
