65b9dd8b5a07db5e4ef1059c113f001ea2d519cd
tasyagamilah12 <anastasiagamilah@students.amikom.ac.id>
2026-07-31 10:24:43 +0700

    Remove logo field from PartnerSeeder

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -9,26 +9,15 @@ class PartnerSeeder extends Seeder
 {
     public function run(): void
     {
-        // Data partner statis yang aman tanpa fungsi fake()
         $partners = [
-            [
-                'name' => 'Direktorat Kemahasiswaan Amikom',
-                'logo' => 'partners/amikom.png',
-            ],
-            [
-                'name' => 'Himpunan Mahasiswa Sistem Informasi',
-                'logo' => 'partners/himasi.png',
-            ],
-            [
-                'name' => 'Kadin Yogyakarta',
-                'logo' => 'partners/kadin.png',
-            ],
+            ['name' => 'Direktorat Kemahasiswaan Amikom'],
+            ['name' => 'Himpunan Mahasiswa Sistem Informasi'],
+            ['name' => 'Kadin Yogyakarta'],
         ];
 
         foreach ($partners as $partner) {
             Partner::firstOrCreate(
-                ['name' => $partner['name']],
-                ['logo' => $partner['logo']]
+                ['name' => $partner['name']]
             );
         }
     }
