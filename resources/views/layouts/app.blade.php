c465494fbb8193489482e57049ae84533a0c6397
tasyagamilah12 <anastasiagamilah@students.amikom.ac.id>
2026-07-31 08:25:34 +0700

    Add rating menu to navbar and update routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,12 +31,15 @@
                 AH</div>
             <span class="text-xl font-bold tracking-tight">AmikomEventHub</span>
         </div>
-        <div class="hidden md:flex gap-8 font-medium">
-            <a href="#" class="text-indigo-600">Jelajahi</a>
+        <div class="hidden md:flex gap-8 font-medium items-center">
+            <a href="{{ route('home') }}" class="text-indigo-600">Jelajahi</a>
             <a href="#" class="hover:text-indigo-600 transition">Kategori</a>
             <a href="#" class="hover:text-indigo-600 transition">Tentang Kami</a>
+            @auth
+                <a href="{{ route('transactions.index') }}" class="hover:text-indigo-600 transition">Rating / Ulasan</a>
+            @endauth
         </div>
-        @auth
+            @auth
             <div class="flex items-center gap-3">
                 @if (Auth::user()->avatar)
                     <img src="{{ Auth::user()->avatar }}" alt="avatar"
